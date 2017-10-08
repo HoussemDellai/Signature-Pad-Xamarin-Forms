871d74b6ddea9070d30cd52e5dedbe25227454db
Houssem Dellai <houssem.dellai@live.com>
2017-10-08 22:48:52 +0200

    Add project files.

---
+++ SignaturePadApp/SignaturePadApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using SignaturePad.Forms;
+using Xamarin.Forms;
+
+namespace SignaturePadApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            Stream image = await PadView.GetImageStreamAsync(SignatureImageFormat.Jpeg);
+        }
+
+        private void ClearButton_Clicked(object sender, EventArgs e)
+        {
+            PadView.Clear();
+        }
+    }
+}
